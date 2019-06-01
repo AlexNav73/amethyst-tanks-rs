f8dfccfb6721462fb8556b86ba2b284ca0cc5ed1
AlexNav73 <alexandr.navitskiy@gmail.com>
2019-06-01 13:41:54 +0300

    Fixed cell size of battlefield's grid

--- src/battlefield.rs
+++ src/battlefield.rs
@@ -1,5 +1,5 @@
 use crate::components::{Grass, Pos, Wall};
-use crate::consts::{BATTLEFIELD_HEIGHT, CELL_HEIGHT, CELL_WIDTH};
+use crate::consts::{BATTLEFIELD_HEIGHT, BATTLEFIELD_WIDTH, CELL_HEIGHT, CELL_WIDTH};
 use crate::map::Map;
 
 use amethyst::{
@@ -8,8 +8,6 @@ use amethyst::{
     prelude::*,
     renderer::{SpriteRender, SpriteSheetHandle},
 };
-
-const SCALE: f32 = 0.01;
 
 pub enum MapCell {
     Wall(Wall),
@@ -27,33 +25,40 @@ impl Battlefield {
     }
 
     pub fn init_grid(self, world: &mut World, sprite_sheet: SpriteSheetHandle) {
-        const WIDTH: f32 = CELL_WIDTH * SCALE;
-
-        let mut top = BATTLEFIELD_HEIGHT - (CELL_HEIGHT * SCALE * 0.5);
+        let grid_width = self.map.first().map(|x| x.len() as f32).unwrap_or(1.0);
+        let scale = BATTLEFIELD_WIDTH / (CELL_WIDTH * grid_width);
+        let width = CELL_WIDTH * scale;
+        let mut top = BATTLEFIELD_HEIGHT - (CELL_HEIGHT * scale * 0.5);
         let mut left;
         for line in self.map {
-            left = WIDTH * 0.5;
+            left = width * 0.5;
             for cell in line {
                 let pos = Pos::new(left, top);
                 match cell {
-                    MapCell::Wall(w) => place(w, pos, 0, world, sprite_sheet.clone()),
-                    MapCell::Grass(g) => place(g, pos, 1, world, sprite_sheet.clone()),
+                    MapCell::Wall(w) => place(w, pos, 0, scale, world, sprite_sheet.clone()),
+                    MapCell::Grass(g) => place(g, pos, 1, scale, world, sprite_sheet.clone()),
                 }
-                left += WIDTH;
+                left += width;
             }
-            top -= CELL_HEIGHT * SCALE;
+            top -= CELL_HEIGHT * scale;
         }
     }
 }
 
-fn place<T>(comp: T, pos: Pos, sprite: usize, world: &mut World, sprite_sheet: SpriteSheetHandle)
-where
+fn place<T>(
+    comp: T,
+    pos: Pos,
+    sprite: usize,
+    scale: f32,
+    world: &mut World,
+    sprite_sheet: SpriteSheetHandle,
+) where
     T: Component + Send + Sync,
 {
     let mut transform = Transform::default();
 
     transform.set_xyz(pos.x, pos.y, 0.0);
-    transform.set_scale(SCALE, SCALE, 0.0);
+    transform.set_scale(scale, scale, 0.0);
 
     let sprite_render = SpriteRender {
         sprite_sheet: sprite_sheet,
--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,5 @@
-pub const BATTLEFIELD_HEIGHT: f32 = 100.0;
-pub const BATTLEFIELD_WIDTH: f32 = 100.0;
+pub const BATTLEFIELD_HEIGHT: f32 = 500.0;
+pub const BATTLEFIELD_WIDTH: f32 = 500.0;
 pub const CELL_HEIGHT: f32 = 512.0;
 pub const CELL_WIDTH: f32 = 512.0;
 pub const WALL_TEXTURE: &'static str = "resources/texture/sprites.png";
